1e051a2fd5910b0d50f552940b09dcf1c6951cf7
Connor Byerman <conbye@cs.utexas.edu>
2023-04-21 16:20:42 -0500

    changes

--- kernel/display.cc
+++ kernel/display.cc
@@ -7,12 +7,16 @@ namespace Display {
        uint8_t* z_buffer = nullptr;
        size_t buffer_size = -1;
        uint8_t* back_buffer = nullptr;
+       const int X_RES = 320;
+       const int Y_RES = 200;
+       const int BYTES_PER_PIXEL;
        
        void init_display() {
               VGA::write_regs(VGA::g_320x200x256);
 
-              buffer_size = 320 * 200 * 4;
+              buffer_size = X_RES * Y_RES * BYTES_PER_PIXEL;
               z_buffer = new uint8_t[buffer_size];
+              for (int i = 0; i < buffer_size; i++) z_buffer[i] = 0;
 
               auto proc = Shared<Process>::make(true);
               thread(proc, [] {
@@ -34,8 +38,14 @@ namespace Display {
               }
        }
 
-       void draw_rect(int x, int y, int width, int height, uint32_t color) {
-
+       void draw_rect(int x, int y, int width, int height, uint8_t color) {
+              if (x + width > 320 || y + height > 200) return;
+              if (x < 0 || y < 0) return;
+              for (int i = x; i < width; i++) {
+                     for (int j = y; j < height; j++) {
+                            VGA::write_pixel8(i, y, color);
+                     }
+              }
        }
 
        void setup_background() {
